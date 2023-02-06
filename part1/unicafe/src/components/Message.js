f678eab143bec2983151d639f8877e154e295525
Sergio <salliana@technisys.com>
2023-02-06 04:08:46 -0300

    Introduction to react: Exercises 1.9-1.10-1.11

---
+++ part1/unicafe/src/components/Message.js
@@ -0,0 +1,12 @@
+import { Title } from "./Title";
+
+const Message = (props) => {
+  return (
+    <div>
+      <Title text={"statistics"} />
+      No feedback given
+    </div>
+  );
+};
+
+export { Message };
--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,3 +1,4 @@
+import { Message } from "./Message";
 import { Statistic } from "./Statistic";
 import { Title } from "./Title";
 
@@ -5,24 +6,33 @@ const Statistics = (props) => {
   const all = props.good + props.neutral + props.bad;
 
   return (
-    <div>
-      <Title text={props.text} />
+    <>
+      {all === 0 ? (
+        <Message />
+      ) : (
+        <div>
+          <Title text={props.text} />
 
-      <table>
-        <tbody>
-          <Statistic text={"good"} value={props.good} />
-          <Statistic text={"neutral"} value={props.neutral} />
-          <Statistic text={"bad"} value={props.bad} />
-          <Statistic text={"all"} value={all} />
-          <Statistic text={"average"} value={(props.good - props.bad) / all} />
-          <Statistic
-            text={"positive"}
-            value={(props.good / all) * 100}
-            symbol={" %"}
-          />
-        </tbody>
-      </table>
-    </div>
+          <table>
+            <tbody>
+              <Statistic text={"good"} value={props.good} />
+              <Statistic text={"neutral"} value={props.neutral} />
+              <Statistic text={"bad"} value={props.bad} />
+              <Statistic text={"all"} value={all} />
+              <Statistic
+                text={"average"}
+                value={(props.good - props.bad) / all}
+              />
+              <Statistic
+                text={"positive"}
+                value={(props.good / all) * 100}
+                symbol={" %"}
+              />
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
   );
 };
 
