3f623ecf7486b0b69a0dce80c9df82155ad5b828
Sergio <salliana@technisys.com>
2023-02-06 18:29:01 -0300

    Communicating with server: Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "0632-4585" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchName, setSearchName] = useState("");
 
   const handleChange = (event) => {
     const eventName = event.target.name;
@@ -14,6 +18,8 @@ const App = () => {
       setNewName(event.target.value);
     } else if (eventName === "number") {
       setNewNumber(event.target.value);
+    } else if (eventName === "search") {
+      setSearchName(event.target.value);
     }
   };
 
@@ -38,6 +44,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter show with:
+        <input
+          type="text"
+          onChange={handleChange}
+          value={searchName}
+          name="search"
+        />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name:
@@ -64,11 +80,15 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => {
-          return (
-            <div key={person.name}>
-              {person.name} {person.number}
-            </div>
-          );
+          if (person.name.toLocaleLowerCase().search(searchName.toLocaleLowerCase()) !== -1) {
+            return (
+              <div key={person.name}>
+                {person.name} {person.number}
+              </div>
+            );
+          } else {
+            return null;
+          }
         })}
       </div>
     </div>
