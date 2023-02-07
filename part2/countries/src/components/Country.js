01be610fae82a3a89e7a3e4134e201ea3ba67f4b
Sergio <salliana@technisys.com>
2023-02-07 02:14:57 -0300

    Communicating with server: Exercise 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState({});
+  const api_key = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+      )
+      .then((response) => {
+        const { current } = response.data;
+        setWeather(current);
+      });
+  }, [api_key, country.capital]);
+
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -19,6 +34,20 @@ const Country = ({ country }) => {
       <br />
       <div>
         <img src={country.flags.png} alt="Country Flag PNG" width="6%" />
+      </div>
+      <div>
+        <h3>Weather in {country.capital}</h3>
+        <div>
+          <div>
+            <div>
+              <strong>temperature: </strong> {weather.temperature} Celcius
+            </div>
+            <img src={weather.weather_icons} alt="Weather Icon" width="5%" />
+            <div>
+              <strong>wind: </strong> {weather.wind_speed} mph direction {weather.wind_dir}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
